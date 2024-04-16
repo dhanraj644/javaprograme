dc5f54c47a3e322c0229d9259be728940ccde784
dhanraj644 <malidhanraj644@gmail.com>
2024-04-16 18:09:44 +0530

    second

--- Greetin.java
+++ Greetin.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 class Greeting 
 {
-	public static void main(Stirng [] args)
+	public static void main(String [] args)
 	{
 	Scanner input =new Scanner(System.in);
 	int a=input.nextInt();
 	int b=input.nextInt();
 
 	int c=a+b;
-
+		
 	System.out.println("Sum is "+c);
 	}	
 }
--- Method.java
+++ Method.java
@@ -3,15 +3,16 @@ import java.util.Arrays;
 public class Method {
     public static void main(String args[])
     {
-       int [] arr={20,5,95,67,5};
+      
 
-       greeting(arr);
-        System.out.println(Arrays.toString(arr));
+    System.out.println(sum(20,30));
+
+    System.out.println(sum(30, 50, 9));
 
     }
-    static int sum(int a, int b ,int)
+    static int sum(int a, int b )
     {
-        return a+b+c;
+        return a+b;
     }
    static int sum(int a, int b, int c)
    {
